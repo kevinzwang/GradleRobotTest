195217baffddbdb8bf4dcc2bb02ca121a3f875a4
Kevin Wamg <20215378+kevinzwang@users.noreply.github.com>
2018-01-21 14:03:00 -0800

    Initial commit

---
+++ src/main/java/org/usfirst/frc/team199/GradleRobotTest/Robot.java
@@ -0,0 +1,30 @@
+package org.usfirst.frc.team199.GradleRobotTest;
+
+import edu.wpi.first.wpilibj.TimedRobot;
+
+public class Robot extends TimedRobot{
+    @Override
+    public void robotInit() {
+        super.robotInit();
+    }
+
+    @Override
+    public void autonomousInit() {
+        super.autonomousInit();
+    }
+
+    @Override
+    public void teleopInit() {
+        super.teleopInit();
+    }
+
+    @Override
+    public void autonomousPeriodic() {
+        super.autonomousPeriodic();
+    }
+
+    @Override
+    public void teleopPeriodic() {
+        super.teleopPeriodic();
+    }
+}
